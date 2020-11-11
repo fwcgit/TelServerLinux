7f9c4836ec4fcabe37efa5f57db9570e677350a9
fwcgit <fu_huaicheng@163.com>
2020-11-11 16:25:36 +0800

    del

--- src/hmsglist.cpp
+++ src/hmsglist.cpp
@@ -1,17 +1,21 @@
 #include "hmsglist.h"
 #include <stdlib.h>
+#include "log.h"
 #include <queue>
 using namespace std;
 queue<package*> msglist;
 void add_msg(package *pk){
+     log_flush("add_msg\r\n");
     msglist.push(pk);
 }
 
 void* get_msg()
 {
     if(msglist.empty()){
+        log_flush("msglist.empty\r\n");
         return (void*)NULL;
     }else{
+        log_flush("!msglist.empty\r\n");
         return msglist.front();
     }
     return (void*)NULL;
